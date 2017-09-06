3b82190fda0b9051fe1971e9f59baf90e9bed3e8
João D. Junior <Joe_cole_23@hotmail.com>
2017-09-06 09:19:49 -0300

    Add files via upload

---
+++ Aula 8/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+	int A = 2, B = 3, *ptrA, *ptrB;
+	
+	ptrA = &A;
+	
+	ptrB = &B;
+	
+	cout<<A<<" "<<&A<<" "<<ptrA<<" "<< *ptrA<<" "<<&ptrA<<endl;
+	
+	*ptrA = *ptrA + *ptrB;
+	
+	cout<<endl;
+	cout<<A<<" "<<&A<<" "<<ptrA<<" "<< *ptrA<<" "<<&ptrA<<endl;
+	
+	return 0;
+}
